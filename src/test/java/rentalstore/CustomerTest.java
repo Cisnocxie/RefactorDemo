d15effb1856fc0493fcb7598a71c40622eb65c9a
XIECI <cisnoc.xie@oocl.com>
2018-08-24 14:41:50 +0800

    movement: add test that renting 1 regular movie for 3 days. Add a test.

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -18,7 +18,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void should_return_correct_html_statement_given_customer_has_rent_one_child_movie_for_4_day() {
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
         Movie regularMovie = new Movie("Titanic", 0);
         Rental oneDayRental = new Rental(regularMovie, 1);
         customer.addRental(oneDayRental);
@@ -33,5 +33,17 @@ public class CustomerTest {
                 "On this rental you earned <EM>1</EM> frequent renter points<P>", statement);
     }
 
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
 
+        String statement = customer.statement();
+
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n" +
+                "Titanic: 3.5<BR>\n" +
+                "<P>You owe<EM>3.5</EM><P>\n" +
+                "On this rental you earned <EM>1</EM> frequent renter points<P>", statement);
+    }
 }
