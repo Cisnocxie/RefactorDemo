9e0dcb03228a7f346c609247623f9ac4af314115
XIECI <cisnoc.xie@oocl.com>
2018-08-24 14:33:43 +0800

    movement: add test that no rental. Add a test.

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -10,6 +10,14 @@ public class CustomerTest {
     private Customer customer = new Customer("Jerry");
 
     @Test
+    public void should_return_correct_statement_given_customer_has_no_rental() {
+        String statement = customer.statement();
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n" +
+                "<P>You owe<EM>0.0</EM><P>\n" +
+                "On this rental you earned <EM>0</EM> frequent renter points<P>", statement);
+    }
+
+    @Test
     public void should_return_correct_html_statement_given_customer_has_rent_one_child_movie_for_4_day() {
         Movie regularMovie = new Movie("Titanic", 0);
         Rental oneDayRental = new Rental(regularMovie, 1);
@@ -24,4 +32,6 @@ public class CustomerTest {
                 "<P>You owe<EM>2.0</EM><P>\n" +
                 "On this rental you earned <EM>1</EM> frequent renter points<P>", statement);
     }
+
+
 }
